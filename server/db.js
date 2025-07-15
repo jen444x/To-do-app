5ba84e41ba016a68c87b79a977732ec2610f40b9
jen444x <106173914+jen444x@users.noreply.github.com>
2025-07-14 17:05:55 -0700

    setup server and db

---
+++ server/db.js
@@ -0,0 +1,18 @@
+// connect to db
+const Pool = require("pg").Pool;
+// importing the Pool class from pg module
+// a pool is a connection pool, it manages multiple db connections
+require("dotenv").config;
+
+// creating a new connection pool with these settings
+const pool = new Pool({
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+});
+
+// how you export code from one file so that it can be imported and used
+// in another file.
+module.export;
---
+++ server/index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express"); // require express library so we can use it.
+// gives you access to the Express functions so you can build a web server.
+// means you're saving that functionality into a variable called express.
+const cors = require("cors"); // import cors middleware
+// CORS stands for Cross-Origin Resource Sharing — it’s a security rule in
+// browsers that can block your frontend from talking to your backend if they’re on
+// different domains/ports (like React on localhost:3000 calling Express on localhost:5050).
+// By requiring it, you’re preparing to use it to avoid those blocks.
+const pool = require("./db"); // i dont have to set up the connection again, just import like so
+
+const app = express(); // var app that is holding express library
+// creating your server instance by calling the express function.
+// app becomes your actual server — this is where you define routes, middleware, and how it handles requests.
+const PORT = process.env.PORT || 5050; // fallback to 5050 if not set
+
+// middleware
+app.use(cors()); // app.use() means: “Every time a request comes in, run this middleware before anything else.”
+// cors() is middleware that modifies the headers on every request so that your backend allows
+// requests from other origins (like your React frontend).
+app.use(express.json()); // makes data available on req.body
+
+// ROUTES
+// create a todo
+
+// route handler that listens for post request in this URl /todos
+// async makes the function asyncrounous so i can use await
+// (req, res) parameters passed in every request
+app.post("/todos", async (req, res) => {
+  try {
+    const description = req.body.description;
+    console.log(description);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// get 1
+// get all
+// delete 1
+// delete multiple
+// delete all
+// edit
+
+app.listen(PORT, () => {
+  // starts the server and tells it to listen for requests on port 5050.
+  // A callback is just a function you pass into another function, to be called later.
+  console.log(`Server has started on port ${PORT}.`);
+});
