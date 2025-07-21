fea5b1d6952a409ff66864ad1362f255c33b5169
jen444x <106173914+jen444x@users.noreply.github.com>
2025-07-21 11:35:29 -0700

    build the input todo component

---
+++ client/src/components/InputTodo.js
@@ -0,0 +1,47 @@
+import React, { Fragment, useState } from "react";
+
+const InputTodo = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { name, description };
+      const response = await fetch("http://localhost:5050/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      console.log(response);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="text-center mt-5">Todo List</h1>
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Task name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Description (optional)"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Add</button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default InputTodo;
