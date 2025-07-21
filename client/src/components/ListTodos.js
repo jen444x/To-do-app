d43277eac8d70ed67b78a39336cbac64c59ffcdc
jen444x <106173914+jen444x@users.noreply.github.com>
2025-07-21 13:28:11 -0700

    Build list todo component

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,47 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5050/todos");
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <Fragment>
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr>
+              <td>{todo.name}</td>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>Delete</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
